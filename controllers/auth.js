1e460890ab20d703bf88e84ea800905b18371223
CTW100 <85342844+CTW100@users.noreply.github.com>
2021-09-26 13:13:16 +0900

    278 Implementing the Token Logic

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,14 +1,19 @@
+const crypto = require("crypto");
+
 const bcrypt = require("bcryptjs");
-// const nodemailer = require("nodemailer");
-// const sendgridTransport = require("nodemailer-sendgrid-transport");
+const nodemailer = require("nodemailer");
+const sendgridTransport = require("nodemailer-sendgrid-transport");
 
 const User = require("../models/user");
 
-// const transporter = nodemailer.createTransport(sendgridTransport({
-//   auth: {
-//     api_key: '~~'
-//   }
-// }))
+const transporter = nodemailer.createTransport(
+  sendgridTransport({
+    auth: {
+      api_key:
+        "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",
+    },
+  })
+);
 
 exports.getLogin = (req, res, next) => {
   let message = req.flash("error");
@@ -94,16 +99,16 @@ exports.postSignup = (req, res, next) => {
         })
         .then((result) => {
           res.redirect("/login");
-          // return transporter.sendMail({
-          //   to: email,
-          //   from: "shop@node-complete.com",
-          //   subject: "Signup succeeded!",
-          //   html: "<h1>You successfully signed up!</h1>",
-          // });
+          return transporter.sendMail({
+            to: email,
+            from: "shop@node-complete.com",
+            subject: "Signup succeeded!",
+            html: "<h1>You successfully signed up!</h1>",
+          });
+        })
+        .catch((err) => {
+          console.log(err);
         });
-      // .catch(err => {
-      //   console.log(err);
-      // });
     })
     .catch((err) => {
       console.log(err);
@@ -128,5 +133,40 @@ exports.getReset = (req, res, next) => {
     path: "/reset",
     pageTitle: "Reset Password",
     errorMessage: message,
+  });
+};
+
+exports.postReset = (req, res, next) => {
+  crypto.randomBytes(32, (err, buffer) => {
+    if (err) {
+      console.log(err);
+      return res.redirect("/reset");
+    }
+    const token = buffer.toString("hex");
+    User.findOne({ email: req.body.email })
+      .then((user) => {
+        if (!user) {
+          req.flash("error", "No account with that email found.");
+          return res.redirect("/reset");
+        }
+        user.resetToken = token;
+        user.resetTokenExpiration = Date.now() + 3600000;
+        return user.save();
+      })
+      .then((result) => {
+        res.redirect("/");
+        transporter.sendMail({
+          to: req.body.email,
+          from: "shop@node-complete.com",
+          subject: "Password reset",
+          html: `
+            <p>You requested a password reset</p>
+            <p>Click this <a href="http://localhost:3000/reset/${token}">link</a> to set a new password.</p>
+          `,
+        });
+      })
+      .catch((err) => {
+        console.log(err);
+      });
   });
 };
